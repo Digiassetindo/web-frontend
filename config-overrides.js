8dfe89c2c63bf8f49d94f6b2c450b2e59079efea
Muhammad Roisul Adib <99470918+roisuladib@users.noreply.github.com>
2022-08-30 11:51:16 +0700

    Remove value Env build domain

--- config-overrides.js
+++ config-overrides.js
@@ -26,12 +26,12 @@ module.exports = function override(config, env) {
             }
         }
 
-        if (`${process.env.BUILD_DOMAIN}` != "") {
-            const domains = process.env.BUILD_DOMAIN.split(',');
-            config.plugins.push(
-                new JavaScriptObfuscator({ rotateUnicodeArray: true, domainLock: domains }, [commonJSFilename])
-            );
-        }
+      //   if (`${process.env.BUILD_DOMAIN}` != "") {
+      //       const domains = process.env.BUILD_DOMAIN.split(',');
+      //       config.plugins.push(
+      //           new JavaScriptObfuscator({ rotateUnicodeArray: true, domainLock: domains }, [commonJSFilename])
+      //       );
+      //   }
 
         config.plugins.push(
             new CompressionPlugin({
